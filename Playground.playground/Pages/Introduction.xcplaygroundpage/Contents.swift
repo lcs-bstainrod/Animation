aeb64684828b05ceace890bd5fe624ec7671ca53
Ben Stainrod <BStainrod@lcs.on.ca>
2020-09-16 10:53:00 -0400

    I have finished all four sides and built a border(:

--- Playground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -7,8 +7,8 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 10 and 11.
  */
-let preferredWidth = 300
-let preferredHeight = 600
+let preferredWidth = 400
+let preferredHeight = 400
 /*:
  ## Required code
  
@@ -35,9 +35,41 @@ PlaygroundPage.current.liveView = canvas
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
  */
 
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
-canvas.drawRectangle(at: Point(x: 50, y: 75), width: 100, height: 200)
-
+// Draw the six lines to start the design (bottom-left corne)
+canvas.drawAxes()
+canvas.drawLine(from: Point(x: 0, y: 7 * 50), to: Point(x: 1 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 6 * 50), to: Point(x: 2 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 5 * 50), to: Point(x: 3 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 4 * 50), to: Point(x: 4 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 3 * 50), to: Point(x:  5 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 2 * 50), to: Point(x: 6 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 1 * 50), to: Point(x: 7 * 50, y: 0))
+canvas.drawLine(from: Point(x: 0, y: 0), to: Point(x: 0, y: 400), lineWidth:15)
+canvas.drawLine(from: Point(x: 0, y: 0), to: Point(x: 400, y: 0), lineWidth:15)
+canvas.drawLine(from: Point(x: 400, y: 0), to: Point(x:400, y: 400), lineWidth:15)
+canvas.drawLine(from: Point(x: 400, y: 400), to: Point(x: 0, y: 400), lineWidth:15 )
+canvas.drawLine(from: Point(x: 400, y: 400), to: Point(x:350, y:0))
+canvas.drawLine(from: Point(x: 400, y: 350), to: Point(x:300, y:0))
+canvas.drawLine(from: Point(x: 400, y: 300), to: Point(x:250, y:0))
+canvas.drawLine(from: Point(x: 400, y: 250), to: Point(x:200, y:0))
+canvas.drawLine(from: Point(x: 400, y: 200), to: Point(x:150, y:0))
+canvas.drawLine(from: Point(x: 400, y: 150), to: Point(x:100, y:0))
+canvas.drawLine(from: Point(x: 400, y: 100), to: Point(x:50, y:0))
+canvas.drawLine(from: Point(x: 400, y: 50), to: Point(x:0, y:0))
+canvas.drawLine(from: Point(x: 0, y: 350), to: Point(x:350, y:400))
+canvas.drawLine(from: Point(x: 0, y: 300), to: Point(x:300, y:400))
+canvas.drawLine(from: Point(x: 0, y: 250), to: Point(x:250, y:400))
+canvas.drawLine(from: Point(x: 0, y: 200), to: Point(x:200, y:400))
+canvas.drawLine(from: Point(x: 0, y: 150), to: Point(x:150, y:400))
+canvas.drawLine(from: Point(x: 0, y: 100), to: Point(x:100, y:400))
+canvas.drawLine(from: Point(x: 0, y: 50), to: Point(x:50, y:400))
+canvas.drawLine(from: Point(x: 350, y: 400), to: Point(x:400, y:50))
+canvas.drawLine(from: Point(x: 300, y: 400), to: Point(x:400, y:100))
+canvas.drawLine(from: Point(x: 250, y: 400), to: Point(x:400, y:150))
+canvas.drawLine(from: Point(x: 200, y: 400), to: Point(x:400, y:200))
+canvas.drawLine(from: Point(x: 150, y: 400), to: Point(x:400, y:250))
+canvas.drawLine(from: Point(x: 100, y: 400), to: Point(x:400, y:300))
+canvas.drawLine(from: Point(x: 50, y: 400), to: Point(x:400, y:350))
 /*:
  ## Show the Assistant Editor
  Don't see any results?
