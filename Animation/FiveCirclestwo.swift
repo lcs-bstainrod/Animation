06007ef52669d18fab27fc462b4fe0e6494fe270
Ben Stainrod <BStainrod@lcs.on.ca>
2020-10-21 14:43:42 -0400

    Finished Five Moving circles

--- Animation/FiveCirclestwo.swift
+++ Animation/FiveCirclestwo.swift
@@ -45,13 +45,13 @@ class FiveCircless: NSObject, Sketchable {
      
         x += 1
         // Draw an ellipse in the middle of the canvas
-        //top line moving right
+        //top line moving right ++
         canvas.drawEllipse(at: Point(x: x, y: 250), width: 50, height: 50)
         canvas.fillColor = Color(hue: 274, saturation: 100, brightness: 100, alpha: 100)
         x1 += 1
-        //bottom line moving right
+        //bottom line moving right ++
         canvas.drawEllipse(at: Point(x: x, y: 450), width: 50, height: 50)
-        canvas.fillColor = Color(hue: 94, saturation: 100, brightness: 100, alpha: 100)
+        canvas.fillColor = Color(hue: 00, saturation: 0, brightness: 0, alpha: 100)
         x2 += 1
         //middle line moving right ++
         canvas.drawEllipse(at: Point(x: x2, y: 50), width: 50, height: 50)
@@ -59,10 +59,10 @@ class FiveCircless: NSObject, Sketchable {
        x3 -= 1
         // top line moving left ++
         canvas.drawEllipse(at: Point(x: x3, y: 350), width: 50, height: 50)
-        canvas.fillColor = Color(hue: 243, saturation: 100, brightness: 100, alpha: 100)
+        canvas.fillColor = Color(hue: 250, saturation: 100, brightness: 100, alpha: 100)
         // bottom line moving left
         canvas.drawEllipse(at: Point(x: x3, y: 150), width: 50, height: 50)
-        canvas.fillColor = Color(hue: 216, saturation: 100, brightness: 100, alpha: 100)
+        canvas.fillColor = Color(hue: 94, saturation: 100, brightness: 100, alpha: 100)
     }
     
 }
